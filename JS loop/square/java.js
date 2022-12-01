d716a7c040ab19737ffb4dc2a44e7ca9a674d67c
ochirsukhM <orange.22.ochirsukhmunkhchuluun@gmail.com>
2022-12-01 12:43:11 +0800

    2

---
+++ JS loop/square/java.js
@@ -0,0 +1,15 @@
+let a = prompt("too oruulna uu")
+let b= 0;
+let c=0;
+let y = "*"
+let x= "\n*"
+while(a-1>b){
+   y+= "*"
+   b++;
+}
+while(a-2>c){
+    x+= "\n*"
+    c++;
+}
+
+console.log(y+x)
