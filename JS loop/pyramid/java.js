b615a138f02901df17be75c986546ed0c65fdf3a
ochirsukhM <orange.22.ochirsukhmunkhchuluun@gmail.com>
2022-12-02 10:21:19 +0800

    2
    git status
    cd ..

--- JS loop/pyramid/java.js
+++ JS loop/pyramid/java.js
@@ -1,11 +1,17 @@
-let a= prompt("pyramidiin undur oruulna uu")
-let b="*";
-let c=0;
-let d="*";
-while(c<a)
-{
-    console.log(b)
-    b=b+d+d;
-    c++;
-    
+let a = prompt("pyramidiin undur oruulna uu")
+let b = "*";
+let d = "*";
+let e = "\ "
+let g = "";
+
+
+for (let c = 1; c <= a; c++) {
+    for (let f = 1; f <= (a - c); f++) {
+        g = g + e
+    }
+    for (let h = 0; h < (2 * c - 1); h++) {
+        g = g + d;
+    }
+    g = g + "\n"
 }
+console.log(g)
