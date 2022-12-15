3e8d7140bdb01cf766a73cbd99e04472d534c4b6
ochirsukhM <orange.22.ochirsukhmunkhchuluun@gmail.com>
2022-12-15 12:25:23 +0800

    2

--- mini project/java.js
+++ mini project/java.js
@@ -3,7 +3,7 @@ let b = document.createElement("h1")
 let c = document.createElement("p")
 let d = new (Date)
 let e = d.getFullYear()
-let f = d.getMonth()
+let f = d.getMonth()+1
 let g = d.getDate()
 let formEl = document.createElement("form")
 let inputEl = document.createElement("input")
@@ -13,12 +13,10 @@ let taskEl = document.createElement("ul")
 
 
 
-
-
 b.innerText = "Todo List App"
 a.appendChild(b)
 
-c.innerHTML = `Date: ${e}-${f}-${g} ${countTasks} tasks left`
+c.innerHTML = `Date: ${e}-${f}-${g} ${document.getElementsByTagName("li").length} tasks left`
 a.appendChild(c)
 
 taskEl.className = "task"
@@ -39,15 +37,45 @@ buttonEl.addEventListener("click", addTask)
 function addTask(event) {
    
     let newTask=document.createElement("li")
+    let editBtn=document.createElement("button")
+    let checkBtn=document.createElement("button")
+    let deleteBtn=document.createElement("button")
+    let editEl=document.createElement("img")
+    let checkEl=document.createElement("img")
+    let deleteEl=document.createElement("img")
+    
+    editEl.src="edit.svg"
+    checkEl.src="check.png"
+    deleteEl.src="delete.png"
+    
+    editEl.className="icons"
+    checkEl.className="icons"
+    deleteEl.className="icons"
+    
     let x=inputEl.value
     newTask.className="newTask"
     newTask.innerHTML=x
-    console.log(x)
+    
     taskEl.appendChild(newTask)
+    newTask.appendChild(editBtn)
+    editBtn.appendChild(editEl)
+    newTask.appendChild(checkBtn)
+    checkBtn.appendChild(checkEl)
+    newTask.appendChild(deleteBtn)
+    deleteBtn.appendChild(deleteEl)
+
+    deleteBtn.addEventListener("click",deleteTask)
+    function deleteTask(event){
+        taskEl.removeChild(newTask)
+        event.preventDefault()
+    }
+    inputEl.value=""
     event.preventDefault();
 }
-function countTasks(z){
+function countTasks(){
 let y=document.getElementsByTagName(li)
 z=y.length
 return z
 }
+
+
